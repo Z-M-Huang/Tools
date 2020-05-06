fbc9130eccb2f16863e99b354d2ce1d2c307ac7a
ZHCode <admin@zh-code.com>
2020-05-05 20:49:01 -0400

    Add animation close

--- assets/js/global.js
+++ assets/js/global.js
@@ -14,3 +14,27 @@ function clearCookies() {
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
+
+function showAlertDanger(message) {
+  $(".alert .alert-dismissible").hide();
+  $("alertDangerMessage").innerHTML = message;
+  $("#alertDanger").slideToggle();
+}
+
+function showAlertWarning(message) {
+  $(".alert .alert-dismissible").hide();
+  $("alertWarningMessage").innerHTML = message;
+  $("#alertWarning").slideToggle();
+}
+
+function showAlertSuccess(message) {
+  $(".alert .alert-dismissible").hide();
+  $("alertSuccessMessage").innerHTML = message;
+  $("#alertSuccess").slideToggle();
+}
+
+function showAlertInfo(message) {
+  $(".alert .alert-dismissible").hide();
+  $("alertInfoMessage").innerHTML = message;
+  $("#alertInfo").slideToggle();
+}
