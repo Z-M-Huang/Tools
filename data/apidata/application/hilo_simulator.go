9e0bb2207a999d8a6fb76726e0a16eab746a467c
Z-M-Huang <25124355+Z-M-Huang@users.noreply.github.com>
2020-05-14 19:56:41 -0400

    Add binding

--- data/apidata/application/hilo_simulator.go
+++ data/apidata/application/hilo_simulator.go
@@ -2,23 +2,23 @@ package application
 
 //HiLoSimulateRequest simulation request
 type HiLoSimulateRequest struct {
-	TotalStack             string `json:"totalStack"`
-	WinChance              string `json:"winChance"`
-	Odds                   string `json:"odds"`
-	BaseBet                string `json:"baseBet"`
-	RollAmount             string `json:"rollAmount"`
-	OnWinReturnToBaseBet   string `json:"onWinReturnToBaseBet"`
-	OnWinIncreaseBy        string `json:"onWinIncreaseBy"`
-	OnWinChangeOdds        string `json:"onWinChangeOdds"`
-	OnWinChangeOddsTo      string `json:"onWinChangeOddsTo"`
-	OnWinNewOddsWinChance  string `json:"onWinNewOddsWinChance"`
-	OnLossReturnToBaseBet  string `json:"onLossReturnToBaseBet"`
-	OnLossIncreaseBy       string `json:"onLossIncreaseBy"`
-	OnLossChangeOdds       string `json:"onLossChangeOdds"`
-	OnLossChangeOddsTo     string `json:"onLossChangeOddsTo"`
-	OnLossNewOddsWinChance string `json:"onLossNewOddsWinChance"`
-	RandomClientSeed       string `json:"randomClientSeed"`
-	AlternateHiLo          string `json:"alternateHiLo"`
+	TotalStack             string `json:"totalStack" xml:"totalStack" form:"totalStack" binding:"required"`
+	WinChance              string `json:"winChance" xml:"winChance" form:"winChance" binding:"required"`
+	Odds                   string `json:"odds" xml:"odds" form:"odds" binding:"required"`
+	BaseBet                string `json:"baseBet" xml:"baseBet" form:"baseBet" binding:"required"`
+	RollAmount             string `json:"rollAmount" xml:"rollAmount" form:"rollAmount" binding:"required"`
+	OnWinReturnToBaseBet   string `json:"onWinReturnToBaseBet" xml:"onWinReturnToBaseBet" form:"onWinReturnToBaseBet"`
+	OnWinIncreaseBy        string `json:"onWinIncreaseBy" xml:"onWinIncreaseBy" form:"onWinIncreaseBy"`
+	OnWinChangeOdds        string `json:"onWinChangeOdds" xml:"onWinChangeOdds" form:"onWinChangeOdds"`
+	OnWinChangeOddsTo      string `json:"onWinChangeOddsTo" xml:"onWinChangeOddsTo" form:"onWinChangeOddsTo"`
+	OnWinNewOddsWinChance  string `json:"onWinNewOddsWinChance" xml:"onWinNewOddsWinChance" form:"onWinNewOddsWinChance"`
+	OnLossReturnToBaseBet  string `json:"onLossReturnToBaseBet" xml:"onLossReturnToBaseBet" form:"onLossReturnToBaseBet"`
+	OnLossIncreaseBy       string `json:"onLossIncreaseBy" xml:"onLossIncreaseBy" form:"onLossIncreaseBy"`
+	OnLossChangeOdds       string `json:"onLossChangeOdds" xml:"onLossChangeOdds" form:"onLossChangeOdds"`
+	OnLossChangeOddsTo     string `json:"onLossChangeOddsTo" xml:"onLossChangeOddsTo" form:"onLossChangeOddsTo"`
+	OnLossNewOddsWinChance string `json:"onLossNewOddsWinChance" xml:"onLossNewOddsWinChance" form:"onLossNewOddsWinChance"`
+	RandomClientSeed       string `json:"randomClientSeed" xml:"randomClientSeed" form:"randomClientSeed"`
+	AlternateHiLo          string `json:"alternateHiLo" xml:"alternateHiLo" form:"alternateHiLo"`
 }
 
 //HiLoVerifyRequest verify request
